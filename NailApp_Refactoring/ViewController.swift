eb658ddfdd63d8265ba83d7302bdff622df104ec
DaichiSaito <h1a_ble@yahoo.co.jp>
2016-04-30 11:36:12 +0900

    Initial Commit

---
+++ NailApp_Refactoring/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NailApp_Refactoring
+//
+//  Created by DaichiSaito on 2016/04/30.
+//  Copyright © 2016年 DaichiSaito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
